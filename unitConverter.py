cdd9a958796a4134293bc1cbaa17931e34e26528
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2017-09-13 13:58:22 -0400

    Updated from Brython Server: 9/13/2017 1:58:21 PM

--- unitConverter.py
+++ unitConverter.py
@@ -1,1 +1,19 @@
+#Matthew Siegel
+#9/13/17
+#unitConverter.py - converts units to different units
+
+numChose = int(input('1 for Km to miles, 2 for Kg to pounds, 3 for liters to gallons, 4 for celsius to fahrenheit'))
+
+if numChose/1 ==1:
+    Km = float(input('Enter a distance in Km: '))
+    print('You have',Km*0.621371, 'miles')
+if numChose/1 ==1:
+    Km = float(input('Enter a distance in Km: '))
+    print('You have',Km*0.621371, 'miles')
+if numChose/1 ==1:
+    Km = float(input('Enter a distance in Km: '))
+    print('You have',Km*0.621371, 'miles')
+if numChose/1 ==1:
+    Km = float(input('Enter a distance in Km: '))
+    print('You have',Km*0.621371, 'miles')
 
