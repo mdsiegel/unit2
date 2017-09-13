720b59a3d1086c5ce2edc332d4379437642e04de
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2017-09-13 13:47:13 -0400

    Updated from Brython Server: 9/13/2017 1:47:11 PM

--- gradeCalculator.py
+++ gradeCalculator.py
@@ -1,1 +1,16 @@
+#Matthew Siegel
+#9/13/17
+#gradeCalculator.py - calculates your grade
 
+grade = float(input('What is your grade? '))
+
+if grade >= 90:
+    print('You have an A')
+elif grade >= 80:
+    print('You have a B')
+elif grade >=70:
+    print('You have a C')
+elif grade >=60:
+    print('You have a D')
+else:
+    print('F')
