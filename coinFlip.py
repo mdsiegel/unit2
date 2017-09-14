a19a51d5571a984f71865d2e7bc920c5ab18791f
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2017-09-14 14:51:05 -0400

    Updated from Brython Server: 9/14/2017 2:51:03 PM

--- coinFlip.py
+++ coinFlip.py
@@ -1,1 +1,10 @@
+#Matthew Siegel
+#9/13/17
+#coinFlip.py - random coinFlip
+from random import randint
 
+num = randint(1,2)
+if num == 1:
+    print(name, "Heads")
+if num == 2:
+    print(name, "Tails")
