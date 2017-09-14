f4d5c2890f017f3646659acadcc7ca9874631b0a
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2017-09-14 14:21:04 -0400

    Updated from Brython Server: 9/14/2017 2:21:01 PM

--- compoundConditionalDemo.py
+++ compoundConditionalDemo.py
@@ -7,3 +7,7 @@ if num>0 and num%7 == 0:
     print('Your number is positive and divisible by 7')
 elif num>0:
     print('Your number is positive and not divisible by 7 :(')
+elif num<0 and num%7 == 0::
+    print('Your number is negative and divisible by 7')
+else:
+    print('Your number is negative and not divisible by 7')
