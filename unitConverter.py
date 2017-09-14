53ce58f7541a790118224a3b857b075280f192f6
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2017-09-14 14:24:31 -0400

    Updated from Brython Server: 9/14/2017 2:24:29 PM

--- unitConverter.py
+++ unitConverter.py
@@ -7,13 +7,13 @@ numChose = int(input('1 for Km to miles, 2 for Kg to pounds, 3 for liters to gal
 if numChose/1 ==1:
     Km = float(input('Enter a distance in Km: '))
     print('You have',Km*0.621371, 'miles')
-if numChose/2 ==2:
+if numChose/2 ==1:
     Kg = float(input('Enter Kg: '))
-    print('You have',kg*0.621371, 'pounds')
-if numChose/3 ==3:
+    print('You have',kg*2.20462, 'pounds')
+if numChose/3 ==1:
     liters = float(input('Enter lites: '))
     print('You have',liters*0.621371, 'gallons')
-if numChose/4 ==4:
+if numChose/4 ==1:
     celsius = float(input('Enter a temp in celsius': '))
     print('You have',celsius*0.621371, 'fahrenheit')
 
