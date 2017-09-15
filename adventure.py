2f663f885b3e083754293a2f2cd39de50a58cfa9
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2017-09-15 14:19:17 -0400

    Updated from Brython Server: 9/15/2017 2:19:13 PM

--- adventure.py
+++ adventure.py
@@ -2,4 +2,6 @@
 #9/13/17
 #adventure.py - like the adventure game
 
-print('You are going on a fun hike. All of a sudden, you see a strange man in the distnace. Do you 
+print('You are going on a fun hike. All of a sudden, you see a strange man in the distnace. Do you run away?')
+if input('yes or no') == 'yes':
+    
