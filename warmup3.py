9d6f26992516451cc628883eb7fa34b96080751c
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2017-09-15 14:22:30 -0400

    Updated from Brython Server: 9/15/2017 2:22:26 PM

--- warmup3.py
+++ warmup3.py
@@ -1,1 +1,16 @@
+#Matthew Siegel
+#9/13/17
+#warmup3.py - seeing what numbers are divisible by
+
+num = int(input('input an integer: '))
+
+if num%2 == 0 and num%3 == 0:
+    print('Divisible by 2 and 3')
+elif num%2 == 0:
+    print('Divisible by 2')
+elif num%3 == 0:
+    print('Divisible by 3')
+else :
+    print('Divisible by neither
+
 
