86080001fb9f4046771763925ad75233bf5960e0
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2017-09-15 14:31:49 -0400

    Updated from Brython Server: 9/15/2017 2:31:45 PM

--- adventure.py
+++ adventure.py
@@ -12,5 +12,19 @@ if input('yes or no') == 'yes':
         if input('yes or no')=='yes':
             if input('Do you through it at his head or body') == 'head':
                 print('You kill the zombie. YAY')
-                
+            else:
+                print('The zombie does not get affected, and eats you. TO BAD')
+        else:
+            print('The zombie attacks you and eats you')
+else:
+    print('You slowly approach him. You begin to relise that he is a zombie. Do you attack him or try to reason with it.')
+    if input('yes or no') == 'yes':
+        print('You stupid idiot. Zombies cannot talk. HE EATS YOU')
+    else:
+        print('There is a knife on the ground. Do you try to stab him in the head or heart?')
+        if input('head or heart')=='head':
+            print('You kill the zombie. YAY')
+        else:
+            print('Dummie, zombies do not have hearts. He eats yours though.')
+        
             
