aa7e6dd4785d5431731f90f768649847191a9aed
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2017-09-15 14:54:53 -0400

    Updated from Brython Server: 9/15/2017 2:54:49 PM

--- lastname.py
+++ lastname.py
@@ -1,1 +1,9 @@
+#Matthew Siegel
+#9/13/17
+#lastname.py- sees where the name falls in the alphabet
 
+name = input('Enter your lastname: ')
+if name>'M':
+    print('Your name falls in the last half of the alphabet')
+else:
+    print('Your name falls in the first half of the alphabet')
