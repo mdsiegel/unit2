a5d58c83b5972575d7a87d37637dbec8dea97dae
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2017-09-15 14:24:37 -0400

    Updated from Brython Server: 9/15/2017 2:24:33 PM

--- adventure.py
+++ adventure.py
@@ -4,4 +4,8 @@
 
 print('You are going on a fun hike. All of a sudden, you see a strange man in the distnace. Do you run away?')
 if input('yes or no') == 'yes':
-    
+    print('You look behind you and see him chasing you. Do you keep running or try to talk with him?')
+    if input('run or talk') == 'run':
+        print('He comes up behind you and eats your face off. THE END')
+    else:
+        print('
