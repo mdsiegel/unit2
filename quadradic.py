9749046e13751f71d302b05a34551ec0b6e5dc58
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2017-09-18 14:53:50 -0400

    Updated from Brython Server: 9/18/2017 2:53:48 PM

--- quadradic.py
+++ quadradic.py
@@ -1,1 +1,27 @@
+#Matthew Siegel
+#9/18/17
+#quadradic.py - solves quadradics
+
+print('ax^2 + bx + c = 0')
+
+
+a = int(input('a: '))
+b = int(input('b: '))
+c = int(input('c: '))
+
+if (b**2 - 4*(a)*(c)) >= 0:
+    x1 = (-b + (b**2 - 4*(a)*(c))**.5)/2*(a)
+    x2 = (-b - (b**2 - 4*(a)*(c))**.5)/2*(a)
+    if x1 != x2:
+        print('x =',x1, 'or x =',x2 )
+    else:
+        print('x =',x1)
+else:
+    x1 = (-b/(2*a))
+    i1 = (-1*(b**2 - 4*(a)*(c)))**.5
+    i2 = -((-1*(b**2 - 4*(a)*(c)))**.5)
+    print('x =',x1,'+',i1, 'i', 'or x =',x1,'+',i2, 'i' )
+  
+    
+
 
