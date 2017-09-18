5447a03c66be5d3f68b4261075a2b65944754a80
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2017-09-18 14:31:54 -0400

    Updated from Brython Server: 9/18/2017 2:31:52 PM

--- ageCalculator.py
+++ ageCalculator.py
@@ -8,4 +8,10 @@ year = int(input('Enter the year you were born: '))
 month = int(input('Enter the month you were born: '))
 day = int(input('Enter the day you were born: '))
 
-
+yearsAlive = date.today().year - year
+if month < date.today().month:
+    print('You are',yearsAlive - 1,'years old')
+if month > date.today().month:
+     print('You are',yearsAlive,'years old')
+if month == date.today().month:
+    
