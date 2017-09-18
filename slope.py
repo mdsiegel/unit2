2c5a51d9c321eeeff8088e64c42f954e5b3b75ab
mdsiegel <31485801+mdsiegel@users.noreply.github.com>
2017-09-18 14:25:14 -0400

    Updated from Brython Server: 9/18/2017 2:25:13 PM

--- slope.py
+++ slope.py
@@ -1,1 +1,18 @@
+#Matthew Siegel
+#9/18/17
+#slope.py - finding the slope of a line
 
+
+x1 = int(input("x1"))
+y1 = int(input("y1"))
+x2 = int(input("x2"))
+y2 = int(input("y2"))
+
+if x1 == x2:
+    print('The slope is undifined')
+    print('X =',x1)
+else:
+    slope = (y2-y1)/(x2-x1)
+    b = y1 - (slope * x1)
+    print("The slope is" , slope)
+    print("The equation for the line is Y =", slope, "x +",b)
